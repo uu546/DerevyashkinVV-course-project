33afd86d3cd8ec14607ad08dbdf7dad3acde279f
uu <qqwe85413@gmail.com>
2026-04-14 15:37:44 +0300

    Создать проект, добавить структуру решения

---
+++ warehouse-management-api/src/main/java/project/warehouse/WarehouseManagementApiApplication.java
@@ -0,0 +1,13 @@
+package project.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManagementApiApplication.class, args);
+    }
+
+}
---
+++ warehouse-management-api/src/test/java/project/warehouse/WarehouseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
